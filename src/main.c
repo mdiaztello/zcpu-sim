e7a23306e3332deba6d131316a12beeb51bbb6e6
Michael Diaz-Tello <mdiaz.tello@gmail.com>
2015-11-08 00:15:58 -0600

    Fixed my test program that draws a box on the screen. Because of the way I was loading values (clearing a register and then doing an ADD_IMMEDIATE) there were times where adjusting the starting location inside the framebuffer would cause the simulator to segfault. I thought it was a bug with the simulator and tried to track it down only to realize that I was mis-using the ADD_IMMEDIATE instruction to work around the fact that LOAD instructions in my preprocessor assembler need to have their LABELs manually calculated (and I _really_ wanted to avoid doing that).
    The specific cause of the bug was that the immediate value field is a
    signed 15-bit quantity and certain locations in the framebuffer map to
    negative 15-bit quantities, which then get sign-extended to very large
    values. When the CPU tries to store a pixel value at this very large
    memory address, the program segfaults because I don't have a complete
    4GB address space allocated for the simulator to use.
    
    All of this just underscores the fact that I need to get both a real
    assembler and a real compiler up and running ASAP.

--- src/main.c
+++ src/main.c
@@ -158,49 +158,53 @@ uint32_t program[] = {
 #endif
 
 
+#include "memory_map.h"
 
 //check writing to the framebuffer with branching by writing a 6x6 blue square
-#define FRAME_BUFFER_START (0x1100)
+#define FRAME_BUFFER_START (GRAPHICS_REGION_START)
 #define BLUE (0x0000FFFF)
+#define GREEN (0x00FF00FF)
+#define RED (0xFF0000FF)
 #define SCREEN_WIDTH 640
-#define BOX_HEIGHT  100
+#define SCREEN_HEIGHT 480
 #define BOX_WIDTH   100
-#define STARTING_POSITION_IN_FRAME_BUFFER   (320)
+#define BOX_HEIGHT  100
+#define STARTING_POSITION_IN_FRAME_BUFFER    (((SCREEN_HEIGHT/2) - (BOX_HEIGHT/2))*SCREEN_WIDTH + ((SCREEN_WIDTH/2) - (BOX_WIDTH/2)))
+
+#define PIXEL_POSITION R1
+#define PIXEL_VALUE R2
+#define BOX_WIDTH_COUNTER R4
+#define BOX_HEIGHT_COUNTER R5
+#define INC(x) ((ADD_IMMEDIATE((x),(x),1)))
+#define DEC(x) ((ADD_IMMEDIATE((x),(x),-1)))
 uint32_t program[] = {
-    CLEAR(R0),      //the start of the frame buffer
-    LOADA(R0, FRAME_BUFFER_START),
-    CLEAR(R1),      //the particular pixel index
-    ADD_IMMEDIATE(R1, R1, STARTING_POSITION_IN_FRAME_BUFFER),
-    ADD_IMMEDIATE(R1, R0, R1),                  //the absolute memory address of our starting position
-    CLEAR(R2),      //the pixel value to write
-    OR_IMMEDIATE(R2, R2, BLUE),
-    CLEAR(R3),      //the screen width
-    ADD_IMMEDIATE(R3, R3, SCREEN_WIDTH),
-    CLEAR(R4),      //the box width
-    ADD_IMMEDIATE(R4, R4, BOX_WIDTH),
-    CLEAR(R5),      //the box height
-    ADD_IMMEDIATE(R5, R5, BOX_HEIGHT),
+    //skip over program data: we have to do this for now because we always
+    //begin execution at location 0
+    BRA(4),
 
-    //FIXME: why doesn't the box appear staringt somewhere other than the upper
-    //left corner? No matter where i set the starting position, it doesn't seem
-    //to set the correct pixel...
-    
-    AND_IMMEDIATE(R1, R1, 0x00),
-    ADD_IMMEDIATE(R1, R1, STARTING_POSITION_IN_FRAME_BUFFER),
-    ADD_IMMEDIATE(R1, R0, R1),                  //the absolute memory address of our starting position
-    ADD(R1, R1, R3),    //go to next screen row
-    STORER(R2, R1, 0),
-    ADD_IMMEDIATE(R1, R1, 1), //inc pixel position
+    //program data
+    BOX_WIDTH,
+    BOX_HEIGHT,
+    STARTING_POSITION_IN_FRAME_BUFFER,
+    RED | GREEN | BLUE,
 
-    ADD_IMMEDIATE(R4, R4, -1), //decrement box column
+    //program begins true execution here
+    LOAD(PIXEL_POSITION, -3),       //STARTING_POSITION_IN_FRAME_BUFFER
+    LOAD(PIXEL_VALUE, -3),          //BLUE
+    LOAD(BOX_WIDTH_COUNTER, -7),    //BOX_WIDTH
+    LOAD(BOX_HEIGHT_COUNTER, -7),   //BOX_HEIGHT
+
+    //Draw the box
+    STORER(PIXEL_VALUE, PIXEL_POSITION, FRAME_BUFFER_START),
+    INC(PIXEL_POSITION),
+    DEC(BOX_WIDTH_COUNTER),
     BRP(-4),
-    ADD_IMMEDIATE(R4, R4, BOX_WIDTH),
-    ADD_IMMEDIATE(R3, R3, SCREEN_WIDTH),    
-    ADD_IMMEDIATE(R5, R5, -1),  //decrement box row
-    BRP(-12),
-
-    BRA(-26),       //loop back to beginning
-
+    ADD_IMMEDIATE(BOX_WIDTH_COUNTER, BOX_WIDTH_COUNTER, BOX_WIDTH),
+    ADD_IMMEDIATE(PIXEL_POSITION, PIXEL_POSITION, SCREEN_WIDTH),
+    ADD_IMMEDIATE(PIXEL_POSITION, PIXEL_POSITION, -BOX_WIDTH),
+    DEC(BOX_HEIGHT_COUNTER),
+    BRP(-8),
+    
     HCF
 };
 #undef SCREEN_WIDTH
